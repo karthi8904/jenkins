5dc5624b92ee8ecb10267a8cf67524e75f68f12d
karthi8904 <70379578+karthi8904@users.noreply.github.com>
2023-08-24 22:19:49 +0530

    Update githubandjenkins.java

--- githubandjenkins.java
+++ githubandjenkins.java
@@ -5,6 +5,7 @@ public static void main (String [] args){
 System.out.println("--------- Jenkins Github----------");
 System.out.println("Hello Jenkins And Github Im Entered");
 System.out.println("Karthi Learning Jenkins");
+System.out.println("Check");
 }
 
 
